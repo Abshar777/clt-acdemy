7a828e7106358c60c0faad9abec94d42bba66cc9
Abshar777 <absharameen625@gmail.com>
2025-11-28 17:18:02 +0530

    doen

--- src/const/services.ts
+++ src/const/services.ts
@@ -9,22 +9,22 @@ export const services = [
     {
         id: 2,
         name: "Discord Trading Channel",
-        mainTitle: "Digital Trading Journal – Turn Every Trade into a Lesson",
-        q: "What You Don’t Track, You Can’t Improve",
-        desc: "Most traders jump from one setup to another without ever analyzing what’s working — and what’s not. Our Digital Trading Journal transforms your habits by giving you data-rich clarity on every move you make in the market."
+        mainTitle: "Discord Trade Channel – Learn In Real Time",
+        q: "Welcome to Your New Trading Floor",
+        desc: "CLT’s premium Discord is where education meets execution. Trade calls aren’t just posted — they’re explained. You’ll understand the logic, the risk, the entry, and the exit — and why."
     },
     {
         id: 3,
         name: "Clt Precision Indicator",
-        mainTitle: "Digital Trading Journal – Turn Every Trade into a Lesson",
-        q: "What You Don’t Track, You Can’t Improve",
-        desc: "Most traders jump from one setup to another without ever analyzing what’s working — and what’s not. Our Digital Trading Journal transforms your habits by giving you data-rich clarity on every move you make in the market."
+        mainTitle: "CLT Precision Indicator  Trade With Clarity",
+        q: "A Smarter Screen. A Sharper Setup.",
+        desc: "Ever struggled to filter noise in the market? The CLT Precision Indicator highlights key liquidity zones, institutional footprints, and imbalance areas so you can act with conviction — not guesswork."
     },
     {
         id: 4,
         name: "Lifetime Mentorship",
-        mainTitle: "Digital Trading Journal – Turn Every Trade into a Lesson",
-        q: "What You Don’t Track, You Can’t Improve",
-        desc: "Most traders jump from one setup to another without ever analyzing what’s working — and what’s not. Our Digital Trading Journal transforms your habits by giving you data-rich clarity on every move you make in the market."
+        mainTitle: "Lifetime Mentorship – Trade With Us for Life",
+        q: "Your Journey is Ours. Always.",
+        desc: "Short-term courses don’t build long-term success. That’s why we offer lifetime mentorship — ongoing calls, trade reviews, updated strategies, and access to every CLT tool, for life."
     }
 ];
