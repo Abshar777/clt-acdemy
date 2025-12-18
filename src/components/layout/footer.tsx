8d5f1a305628b26291eb09012ace37fc103b3d8b
Abshar777 <absharameen625@gmail.com>
2025-12-18 16:48:33 +0530

    done

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -92,8 +92,8 @@ const Footer = () => {
             <div className="space-y-4 text-gray-300">
               <p className="flex items-start gap-3">
                 <IoLocationSharp className="text-xl text-nowrap   mt-1" />
-                CLT Academy – Head Office Al Qiyadah Al Shaibani Building, M09
-                Hor Al Anz East Dubai United Arab Emirates
+                CLT Academy – Head Office Al&nbsp;Qiyadah Al&nbsp;Shaibani Building, M09
+                Hor Al&nbsp;Anz East Dubai United Arab Emirates
               </p>
               <p className="flex items-center gap-3">
                 <FiPhoneCall className="text-xl" /> +971 557454523
