7bc7b878765c6dce1db260c7561fb66ccd016603
Abshar777 <absharameen625@gmail.com>
2025-12-18 16:42:45 +0530

    done

--- src/components/page-sections/contact/contactUs.tsx
+++ src/components/page-sections/contact/contactUs.tsx
@@ -16,7 +16,7 @@ const contactInfo = [
     type: "Address",
     icon: <FaMapMarkerAlt className="w-6 h-6 text-white" />,
     content:
-      "Al Quiyadh Al Shaibani Building, M09 Hor Al Anz East Dubai United Arab Emirates",
+      "CLT Academy – Head Office Al Qiyadah Al Shaibani Building, M09 Hor Al Anz East Dubai United Arab Emirates",
   },
 ];
 
