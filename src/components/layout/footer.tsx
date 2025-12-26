a08cb629576a7aa81a8302d21d29ea4513b00c07
Abshar777 <absharameen625@gmail.com>
2025-12-26 14:36:17 +0530

    Refactor address formatting in footer component to enhance readability with line breaks.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -92,8 +92,8 @@ const Footer = () => {
             <div className="space-y-4 text-gray-300">
               <p className="flex items-start gap-3">
                 <IoLocationSharp className="text-xl text-nowrap   mt-1" />
-                CLT Academy Head Office M09, Al&nbsp;Shaibani Building,
-                 Hor&bsp;Al&nbsp;Anz&nbsp;East, Dubai, United Arab Emirates.
+                CLT Academy Head Office <br />M09, Al&nbsp;Shaibani Building,
+                Hor&bsp;Al&nbsp;Anz&nbsp;East, Dubai, United Arab Emirates.
               </p>
               <p className="flex items-center gap-3">
                 <FiPhoneCall className="text-xl" /> +971 557454523
