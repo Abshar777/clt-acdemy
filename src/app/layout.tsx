b950329803015981e187f45c6645b51b951232da
abshar <139979587+Abshar777@users.noreply.github.com>
2026-04-14 15:54:14 +0530

    Remove Google verification from layout metadata
    Removed Google verification from metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,9 +33,7 @@ export const metadata: Metadata = {
   title: "CLT Academy",
   description: "CLT Academy is a platform for learning trading and investing.",
   keywords: keywords,
-  verification: {
-    google: "DaVM4iUS9uUd1fAHt13kalio5mwEJB73Q4ZieWKJavQ",
-  },
+
   openGraph: {
     images: [
       {
